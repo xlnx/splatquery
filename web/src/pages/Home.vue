1d5a911da16181c523b4a533f70cf11c76363bc4
koishi <qwq@koishi.top>
2023-07-08 20:23:04 +0800

    fix section bg transition soundless

--- web/src/pages/Home.vue
+++ web/src/pages/Home.vue
@@ -1,47 +1,34 @@
 <template>
-  <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
-      <h1
-        class="font-splatoon1 mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-7xl lg:text-8xl dark:text-white">
-        SplatQuery
-      </h1>
-      <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
-        Get notified about your favourite gear/schedule updates of splatoon 3.
-      </p>
-      <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4" v-if="showLoginBar">
-        <button type="button"
-          class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
-          @click="login.google">
-          <svg class="w-4 h-4 mr-2 -ml-1" focusable="false" data-prefix="fab" data-icon="google" role="img"
-            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
-            <path fill="currentColor"
-              d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
-            </path>
-          </svg>
-          Sign in with Google
-        </button>
-      </div>
-      <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4" v-if="showMain">
-        <a type="button" class="fmt-button" href="query/new">
-          + New Query
-        </a>
-        <a type="button" class="fmt-button" href="query/list">
-          Query List
-        </a>
-      </div>
+  <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+    <h1
+      class="font-splatoon1 mb-4 text-5xl font-extrabold tracking-tight leading-none text-gray-900 md:text-7xl lg:text-8xl dark:text-white">
+      SplatQuery
+    </h1>
+    <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+      Get notified about your favourite gear/schedule updates of splatoon 3.
+    </p>
+    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4" v-if="showLoginBar">
+      <button type="button"
+        class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2"
+        @click="login.google">
+        <svg class="w-4 h-4 mr-2 -ml-1" focusable="false" data-prefix="fab" data-icon="google" role="img"
+          xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512">
+          <path fill="currentColor"
+            d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z">
+          </path>
+        </svg>
+        Sign in with Google
+      </button>
     </div>
-  </section>
-
-  <!-- <div id="toast-simple"
-    class="absolute flex items-center w-full max-w-xs p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800"
-    role="alert">
-    <svg class="w-5 h-5 text-blue-600 dark:text-blue-500 rotate-45" xmlns="http://www.w3.org/2000/svg"
-      fill="none" viewBox="0 0 18 20">
-      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-        d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9" />
-    </svg>
-    <div class="pl-4 text-sm font-normal">Message sent successfully.</div>
-  </div> -->
+    <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4" v-if="showMain">
+      <a type="button" class="fmt-button" href="query/new">
+        + New Query
+      </a>
+      <a type="button" class="fmt-button" href="query/list">
+        Query List
+      </a>
+    </div>
+  </div>
 </template>
 
 <script setup>
